b86f9ae69dbb7c8269741f5000fcc7d2e4d7cc76
hristo-grudev <hr.grudev@gmail.com>
2021-04-19 12:14:10 +0300

    Job is done

--- bsicbank/spiders/spider.py
+++ bsicbank/spiders/spider.py
@@ -12,25 +12,34 @@ from itemloaders.processors import TakeFirst
 class BsicbankSpider(scrapy.Spider):
 	name = 'bsicbank'
 	start_urls = ['http://www.bsicbank.com/']
+	page_link = 'http://www.bsicbank.com//category/%D8%A3%D8%AE%D8%A8%D8%A7%D8%B1'
+	next_page = []
 
 	def parse(self, response):
-		data = requests.request("GET", 'http://www.bsicbank.com//category/%D8%A3%D8%AE%D8%A8%D8%A7%D8%B1')
+		print(self.page_link)
+		data = requests.request("GET", self.page_link)
 		data = scrapy.Selector(text=data.text)
 		post_links = data.xpath('//ul[@class="category-list"]/li/a/@href').getall()
 		for foo in post_links:
 			url = foo.encode('Latin-1').decode('UTF-8')
 			yield response.follow(url, self.parse_post)
 
-		next_page = data.xpath('//a[@class="pagearrow"]/@href').getall()
-		yield from response.follow_all(next_page, self.parse)
+		try:
+			self.page_link = data.xpath('//a[@class="pagearrow"][img[@alt="Next"]]/@href').get().encode('Latin-1')
+		except:
+			return
+		if self.page_link in self.next_page:
+			return
+		else:
+			self.next_page.append(self.page_link)
+		yield response.follow(response.url, self.parse, dont_filter=True)
 
 	def parse_post(self, response):
 		title = response.xpath('//h2/text()').get()
-		print(response)
 		description = response.xpath('//div[@class="blog-post-description"]//text()[normalize-space()]').getall()
 		description = [p.strip() for p in description if '{' not in p]
 		description = ' '.join(description).strip()
-		date = response.xpath('//div[@class="blog-post-time ml-0"]/a/i/text()[normalize-space()]').get()
+		date = response.xpath('//div[@class="blog-post-time ml-0"]/a//text()[normalize-space()]').get()
 
 		item = ItemLoader(item=BsicbankItem(), response=response)
 		item.default_output_processor = TakeFirst()
